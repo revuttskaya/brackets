e3fd670fdc0a234a71c5213681b0d8f0439657b2
revuttskaya <revutskaya.ke@gmail.com>
2021-03-15 23:53:48 +0300

    init: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let closingBracketsIndicesQueue = [];
+    let joinedBracketsConfig = '';
+
+    for (let bracketConfig of bracketsConfig) {
+        for (let bracketConfigElement of bracketConfig) {
+            joinedBracketsConfig += bracketConfigElement;
+        }
+    }
+
+    for (let inputBracket of str) {
+        let indexOfConfigBracket = joinedBracketsConfig.indexOf(inputBracket);
+        if (indexOfConfigBracket === -1) {
+            return false;
+        }
+
+        if (indexOfConfigBracket % 2 === 0) {
+            closingBracketsIndicesQueue.push(indexOfConfigBracket + 1);
+        } else {
+            if (closingBracketsIndicesQueue.pop() !== indexOfConfigBracket) {
+                return false;
+            }
+        }
+    }
+
+    return closingBracketsIndicesQueue.length === 0;
+};
+
